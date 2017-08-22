bbb3232feb237f7b8c3129cc6006c003e51fd5d2
xianicai <zhanglibinit@163.com>
2017-08-22 23:37:45 +0800

    建立项目

---
+++ app/src/main/java/com/example/xianicai/dicegame/base/basemvp/BasePresenter.java
@@ -0,0 +1,13 @@
+package com.example.xianicai.dicegame.base.basemvp;
+
+/**
+ * Created by Zhanglibin on 2017/4/19.
+ */
+
+public interface BasePresenter <T extends BaseView> {
+    void bindView(T view);
+
+    T getView();
+
+    void release();
+}
